b86b6be92fce9842c00a54439454345ee812415b
fcarelse <46908307+fcarelse@users.noreply.github.com>
2021-06-24 22:36:50 +0100

    Update TwitchChatBonusAutoClicker.js

--- TwitchChatBonusAutoClicker.js
+++ TwitchChatBonusAutoClicker.js
@@ -1,6 +1,15 @@
+// Older
 var interval = setInterval(a=>document
 	.querySelector('[data-test-selector="community-points-summary"]')
 	.querySelectorAll('div')
 	.forEach((a, i)=>i<5 || a?.click())
+	,20000
+);
+
+// Current June 2021
+var interval = setInterval(
+	$=>document.querySelector('[data-test-selector="community-points-summary"]')
+	.querySelectorAll('div')
+	.forEach((a, i)=>i==20 || a?.click())
 	,20000
 );

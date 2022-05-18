9f6843ae5cfb2aeee3258dabb6ee0fc5705edfe9
fcarelse <46908307+fcarelse@users.noreply.github.com>
2022-05-18 22:40:47 +0100

    Update TwitchChatBonusAutoClicker.js May2022
    May 2022 For Tampermonkey

--- TwitchChatBonusAutoClicker.js
+++ TwitchChatBonusAutoClicker.js
@@ -6,10 +6,44 @@ var interval = setInterval(a=>document
 	,20000
 );
 
-// Current June 2021
+// June 2021
 var interval = setInterval(
 	$=>document.querySelector('[data-test-selector="community-points-summary"]')
 	.querySelectorAll('div')
 	.forEach((a, i)=>i==20 || a?.click())
 	,20000
 );
+
+// current May 2022 using Tampermonkey
+
+// ==UserScript==
+// @name         Twitch Autoclicker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.twitch.tv/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitch.tv
+// @grant        none
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+
+    window.TwitchAutoclicker= {
+        interval: setInterval(
+            $=>window.TwitchAutoclicker.tryClick()
+            ,10000
+        ),
+        tryClick: function(){
+            document.querySelector('[data-test-selector="community-points-summary"]')
+                .querySelectorAll('div')
+                .forEach((a, i)=>i==20 || a?.click());
+            // Click to close rewards popup.
+            document.querySelectorAll("div.ScPopoverHeaderIconSlot-sc-9cyx3f-1.jOWGtH.tw-popover-header__icon-slot--right button")[1].click();
+        },
+    };
+    console.log('Twitch Autoclicker Activated');
+})();

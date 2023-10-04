5511019c358f78e730e235ec3e6156c43e5096b6
fcarelse <46908307+fcarelse@users.noreply.github.com>
2023-10-04 15:41:24 +0100

    Update YoutubeGridColumns.js

--- YoutubeGridColumns.js
+++ YoutubeGridColumns.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name				 New Userscript
+// @name				 Youtube Grid Columns
 // @namespace		http://tampermonkey.net/
 // @version			0.1
 // @description	try to take over the world!
-// @author			 You
+// @author			 Francis Carelse
 // @match				https://www.youtube.com/*
 // @icon				 data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant				none

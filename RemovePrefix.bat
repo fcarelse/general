d4467d0060b9aa9303bea8e0acde1e37d5bb9402
fcarelse <46908307+fcarelse@users.noreply.github.com>
2024-04-08 02:26:29 +0100

    Update RemovePrefix.bat
    Prefix can include underscore character

--- RemovePrefix.bat
+++ RemovePrefix.bat
@@ -1,6 +1,6 @@
 @echo off
 set temp_file=__temp-RemovePrefix__.bat
-dir /b [*.* |sed "s/^\[[- a-zA-Z]*\][ _]*\(.*\)/ren \"\0\" \"\1\"/">%temp_file%
+dir /b [*.* |sed "s/^\[[- _a-zA-Z]*\][ _]*\(.*\)/ren \"\0\" \"\1\"/">%temp_file%
 call %temp_file%
 del %temp_file%
 
